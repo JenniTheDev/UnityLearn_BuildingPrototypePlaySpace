30bd302a80fce6991f2bbb3dd5f3fff53cb942b9
Jenni <jfelton@csu.fullerton.edu>
2020-08-10 16:53:20 -0700

    Finished Tutorial

--- Assets/System/4. Item Loot/Scripts/Monobehaviours/Lootable.cs
+++ Assets/System/4. Item Loot/Scripts/Monobehaviours/Lootable.cs
@@ -4,29 +4,28 @@ using UnityEngine;
 
 public class Lootable : MonoBehaviour {
 
-	public int minSwingsNeeded = 2;
-	public int maxSwingsNeeded = 6;
+    public int minSwingsNeeded = 2;
+    public int maxSwingsNeeded = 6;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
 
-	public void Loot(int digSwings)
-	{
-		Instantiate (Resources.Load ("DirtEffect"), transform.position, transform.rotation);
-		if (digSwings > Random.Range (minSwingsNeeded, maxSwingsNeeded)) {
-			Instantiate (Resources.Load ("Chest"), transform.position, transform.rotation);
-			Destroy (gameObject);
-		}
-	}
+    }
 
-	/*
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Loot(int digSwings) {
+        Instantiate(Resources.Load("DirtEffect"), transform.position, transform.rotation);
+        if (digSwings > Random.Range(minSwingsNeeded, maxSwingsNeeded)) {
+            Instantiate(Resources.Load("Chest"), transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    /*
 	public void Loot(int digSwings)
 	{
 		if(digSwings > Random.Range(0,4)){
